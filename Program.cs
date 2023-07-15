40961e21f2d77faa153993715bb61dab76f13be5
Aleksander <emelin.ss@mail.ru>
2023-07-15 13:40:36 +0300

    Таск12 Проблема

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿            int Input(string text)
+        {
+            Console.Write(text);
+            return Convert.ToInt32(Console.Readline());
+        }
+
+       
+        void Task12()
+        {
+            // напишите программу, которая будет принимать на вход 2 числа и 
+            //вывадить, является ли второе число кратным первому
+
+            
+            int number_f = Input("Введите первое число:  ");
+
+            int number_s = Input("Введите второе число:  ");
+
+            if (number_s % number_f == 0)
+            {
+                Console.WriteLine($"Число {number_s}  кратно числу {number_f}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number_s} Не кратно числу {number_f}."
+                   + $"Остаток от деления равен {number_s % number_f}");
+            }
+        }
+        Console.Clear();
+    
+        Task12();
